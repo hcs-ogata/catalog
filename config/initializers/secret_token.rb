06c41bd67b0f9cd497740be3da94ac30bbe44531
Syohei OGATA <ogata@hoccs.co.jp>
2012-10-23 15:47:03 +0900

    rails new catalog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catalog::Application.config.secret_token = 'b7d5250ba82b7626c0db3bd5fa76a908f3258abd1b3fae6a035d45a71d98dd0bcc8a6396da605699d05f1012e13f3480f5055eec12da7bc3e560688f96aa44a1'
