01f6a260f99b191930324ee4dcba4b46c40e6c95
Syohei OGATA <ogata@hoccs.co.jp>
2012-10-24 11:00:39 +0900

    [#38241017] 不正な入力に対するエラー処理を追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :image, :image_cache
   mount_uploader :image, ProductImageUploader
+
+  validates_presence_of :name, :description
+  validates_numericality_of :price, :greater_than => 0, :only_integer => true
+
 end
